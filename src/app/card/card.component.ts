e6f4e322b7577e004da08bffef44423753ac1dff
Avanish Patel <patelabout@gmail.com>
2019-01-14 11:04:12 -0600

    Prevented right click on image to stop download

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  images:string[] = ["image1.jpg","image2.jpg","image3.jpg","image1.jpg","image2.jpg","image3.jpg","image1.jpg","image2.jpg","image3.jpg"] 
+  images:string[] = ["1.jpg","10.jpg","9.jpg","2.jpg","3.jpg","5.jpg","6.jpg","7.jpg","8.jpg","11.jpg","12.jpg","13.jpg"] 
 
   constructor() { }
 
